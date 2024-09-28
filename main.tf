00a5738578c8376e6963b2b79ce53528bb888002
shanuvik <vikramp.2104@gmail.com>
2024-09-28 17:26:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,3 @@ provider  "aws" {
 resource "aws_vpc" "myvpc01" {
         cidr_block = "10.0.0.0/16"
         }
-
-resource "aws_subnet" "sub-1" {
-        vpc_id = aws_vpc.myvpc01.id
-        cidr_block = "10.0.1.0/24"
-        }
-
-resource "aws_subnet" "sub-2" {
-        vpc_id = aws_vpc.myvpc01.id
-        cidr_block = "10.0.2.0/24"
-        }
