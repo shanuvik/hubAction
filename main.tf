880b316b090c26f316fbc01f3e5c15138fbcdd8f
shanuvik <vikramp.2104@gmail.com>
2024-09-28 18:00:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,16 @@ provider  "aws" {
         region = "us-west-1"
         }
 
+terraform {
+  backend "s3" {
+    bucket         = "onemyterralockbucket"
+    key            = "TERRAFORM-STATE/terraform.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
 resource "aws_vpc" "myvpc01" {
         cidr_block = "10.0.0.0/16"
         }
