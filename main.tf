bfdbe9cbf578b4430627ad18b59f94ae72f21a16
Ishan Purohit <xyz@gmail.com>
2024-09-29 18:49:39 +0530

    added subnet03

--- main.tf
+++ main.tf
@@ -26,3 +26,7 @@ resource "aws_subnet" "mySub02" {
         cidr_block = "10.0.9.0/24"
         }
 
+resource "aws_subnet" "mySub03" {
+        vpc_id = aws_vpc.myvpc01.id
+        cidr_block = "10.0.3.0/24"
+        }
